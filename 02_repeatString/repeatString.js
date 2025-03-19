094b8547eaabf566fe8bb2c2c05de83c0c9adc6c
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-18 16:49:24 -0800

    complete 4th test,
    returned empty string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function() {
     let finalString = "";
-    for (let i=1; i > 0; i--) {
-        finalString += 'hi';
+    for (let i=0; i > 0; i--) {
+        finalString += 'bye';
     }
 
     return finalString;
