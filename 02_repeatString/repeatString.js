b64e3d660404570681e5c8861eb74815f24b43ad
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-18 16:39:19 -0800

    complete first test
    learned how to repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
+    let finalString = "";
+    for (let i=3; i > 0; i--) {
+        finalString += 'hey';
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
