dacf04e1e771162b5a5516172880e9584b07ae87
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-21 15:19:17 -0800

    complete test 1, utilized a for loop w/conditions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, numToRemove) {
+    // using to push relavent elements from array to new array since .delete leaves holes. 
+    const newArray = [];
+
+    for (num of array) {
+        if (num === numToRemove) {
+            continue;
+
+        } else {
+            newArray.push(num);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
