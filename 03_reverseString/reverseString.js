0e94a6c2a15e06cbaf63a40f1b901bb26dc6b383
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-21 09:32:50 -0800

    Complete first test, succesfully returned reverse string!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
 const reverseString = function() {
-
-};
+    oldString="hello";
+    let newString = "";
+    for (let i=oldString.length-1; i>=0; i--) {
+        newString += oldString.charAt(i);
+    }
+    
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
