8b7586124acf18314699af2790868a3c81096290
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-22 08:24:50 -0800

    solve all tests except one, non-integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,13 @@ const sumAll = function(num1=0, num2=0) {
 
     let sum = 0;
 
-    if (num1 > num2) {
+    if (num1 < 0 || num2 < 0) {  // This works
+        return "ERROR";
+
+    } else if (typeof(num1) != typeof(Number()) || typeof(num2) != typeof(Number())) {  // This does not, why?
+        return "ERROR";
+
+    } else if (num1 > num2) {
         for (let i=num1; i>=num2; i--) {
             sum += i; 
         }
