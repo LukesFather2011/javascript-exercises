d27aca31b8c4822115170fba2f27c08c680a0f21
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-09-27 12:46:25 -0500

    complete exercise, pass all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumOfArray = array.reduce((total, current) => {
+    return total + current;
+  }, 0);
+  return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let productOfArray = array.reduce((total, current) => {
+    return total * current;
+  }, 1);
+  return productOfArray;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let total = 1;
+  for (let i = 0; i < exponent; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	let total = 1;
+  for (let i = int; i != 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
