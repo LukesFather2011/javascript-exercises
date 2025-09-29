d2aaff49b4e0654759701c389409452e1c41e5b6
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-09-29 08:36:37 -0500

    finish exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz0123456789"; // used to check for valid characters
+  let filteredString = "";
 
+  // ensure everything is lower case and remove all invalid characters.
+  for (const char of str.toLowerCase()) {
+    if (validChars.includes(char)) {
+      filteredString += char;
+    }
+  }
+
+  const reversedString = filteredString.split("").reverse().join("");
+  return filteredString === reversedString;
 };
 
 // Do not edit below this line
