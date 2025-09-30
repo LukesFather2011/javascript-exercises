ae5f6a5e3b68eb1b8e0b425a49fee4e0fd616983
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-09-30 07:46:36 -0500

    finish exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    switch (Number(input)) {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            return 1;
+            break;
+    };
 
+    if (Number(input) < 0) return "OOPS";
+
+    let arr = [1, 1];
+    for(let i=2; i < Number(input); i++) {
+        arr[i] = arr[i-2] + arr[i-1];
+    };
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
