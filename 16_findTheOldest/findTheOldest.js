2907efe8529ba0bcfa0966023b59e4be9ec3ebd6
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-10-02 07:46:21 -0500

    Finish exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let oldestPerson = null;
+  let ageOfCurrentOldest = 0;
+  
+  for (let person of array) {
+    // handle case where yearOfDeath is missing (assume still alive)
+    const yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    const age = yearOfDeath - person.yearOfBirth;
 
+    if (age > ageOfCurrentOldest) {
+      ageOfCurrentOldest = age;
+      oldestPerson = person; // store the whole object
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
